facd57f0aa65e74a4de2c365979d408b3db8c3e1
Auston Stewart <auston@shedbuilt.net>
2018-01-22 13:09:13 -1000

    Adding support for Le Potato andupdating to v2018.01

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-UBOOTCFG=defconfig
-BOOTLOADER=u-boot-sunxi-with-spl.bin
 case $SHED_HWCONFIG in
     orangepi-one)
         UBOOTCFG=orangepi_one_defconfig
@@ -10,10 +8,18 @@ case $SHED_HWCONFIG in
         UBOOTCFG=orangepi_pc_defconfig
         BOOTLOADER=u-boot-sunxi-with-spl.bin
         ;;
+    aml-s905x-cc)
+        UBOOTCFG=libretech-cc_defconfig
+        BOOTLOADER=u-boot.bin
+        ;;
+    *)
+        echo "Unsupported config: '$SHED_HWCONFIG'"
+        exit 1
+        ;;
 esac
-make $UBOOTCFG
-make -j $SHED_NUMJOBS
-mkdir -v "${SHED_FAKEROOT}/boot"
-install -m644 "$BOOTLOADER" "${SHED_FAKEROOT}/boot"
+make $UBOOTCFG && \
+make -j $SHED_NUMJOBS || exit 1
+mkdir -pv "${SHED_FAKEROOT}/boot/u-boot"
+install -m644 "$BOOTLOADER" "${SHED_FAKEROOT}/boot/u-boot/${SHED_HWCONFIG}.bin"
 mkdir -v "${SHED_FAKEROOT}/boot/extlinux"
 install -m644 "${SHED_CONTRIBDIR}/extlinux.template" "${SHED_FAKEROOT}/boot/extlinux/"

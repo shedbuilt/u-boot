ad813038e4881018cf9342423f7185fbfb26940f
Auston Stewart <auston@shedbuilt.net>
2018-02-15 23:35:43 -1000

    Reverting to 2018.01, adding workaround for large compressed kernels

--- build.sh
+++ build.sh
@@ -17,8 +17,10 @@ case "$SHED_DEVICE" in
         exit 1
         ;;
 esac
+# Increase default max gunzip size to 16M to accommodate larger kernels
+sed -i 's/#define CONFIG_SYS_BOOTM_LEN.*/#define CONFIG_SYS_BOOTM_LEN 0x1000000/g' common/bootm.c && \
 make $UBOOTCFG && \
 make -j $SHED_NUMJOBS || exit 1
 install -Dm755 tools/mkimage "${SHED_FAKEROOT}/usr/bin/mkimage"
-install -Dm644 "$BOOTLOADER" "${SHED_FAKEROOT}/boot/u-boot/2018.03rc2_${SHED_HWCONFIG}.bin"
+install -Dm644 "$BOOTLOADER" "${SHED_FAKEROOT}/boot/u-boot/2018.01_${SHED_HWCONFIG}.bin"
 install -Dm644 "${SHED_CONTRIBDIR}/extlinux.template" "${SHED_FAKEROOT}/boot/extlinux/extlinux.template"

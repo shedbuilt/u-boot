99e66f3ddb8089ef607e2ab3ca8cd2e9dcb79d8d
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:29:16 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ case "$SHED_DEVICE" in
         ;&
     orangepi-one|orangepi-pc|orangepi-lite|all-h3-cc)
         SHDPKG_BOOTLOADER='u-boot-sunxi-with-spl.bin'
-        patch -Np1 -i "${SHED_PATCHDIR}/u-boot-2018.03-sunxi-no-env.patch" || exit 1
+        patch -Np1 -i "${SHED_PKG_PATCH_DIR}/u-boot-2018.03-sunxi-no-env.patch" || exit 1
         ;;
     aml-s905x-cc)
         SHDPKG_BOOTLOADER='u-boot.bin'
@@ -19,13 +19,13 @@ esac
 
 # Increase default max gunzip size to 16M to accommodate larger kernels
 sed -i 's/#define CONFIG_SYS_BOOTM_LEN.*/#define CONFIG_SYS_BOOTM_LEN 0x1000000/g' common/bootm.c &&
-cp "${SHED_CONTRIBDIR}/${SHED_DEVICE}.config" .config &&
-make -j $SHED_NUMJOBS || exit 1
+cp "${SHED_PKG_CONTRIB_DIR}/${SHED_DEVICE}.config" .config &&
+make -j $SHED_NUM_JOBS || exit 1
 
 # Store the bootloader in /boot so it can be written to MMC in post-install
 if [ "$SHED_DEVICE" == 'orangepi-pc2' ]; then
     cat spl/sunxi-spl.bin u-boot.itb > u-boot-sunxi-with-spl.bin || exit 1
 fi
-install -Dm755 tools/mkimage "${SHED_FAKEROOT}/usr/bin/mkimage" &&
-install -Dm644 "$SHDPKG_BOOTLOADER" "${SHED_FAKEROOT}/boot/u-boot/2018.03_${SHED_DEVICE}.bin" &&
-install -Dm644 "${SHED_CONTRIBDIR}/extlinux.sample" "${SHED_FAKEROOT}/boot/extlinux/extlinux.sample"
+install -Dm755 tools/mkimage "${SHED_FAKE_ROOT}/usr/bin/mkimage" &&
+install -Dm644 "$SHDPKG_BOOTLOADER" "${SHED_FAKE_ROOT}/boot/u-boot/2018.03_${SHED_DEVICE}.bin" &&
+install -Dm644 "${SHED_PKG_CONTRIB_DIR}/extlinux.sample" "${SHED_FAKE_ROOT}/boot/extlinux/extlinux.sample"

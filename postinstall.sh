bef3c9266a19b33c0c210f26bf7cc93cb740d5a8
Auston Stewart <auston@shedbuilt.net>
2018-04-27 08:52:00 -1000

    Fixing bad NanoPi M1 Plus environment config and adding warnings.

--- postinstall.sh
+++ postinstall.sh
@@ -2,3 +2,5 @@
 if [ ! -e /boot/extlinux/extlinux.conf ]; then
     install -m644 /boot/extlinux/extlinux.sample /boot/extlinux/extlinux.conf
 fi
+echo "The u-boot bootloader has been installed to /boot/u-boot/2018.03_${SHED_DEVICE}.bin"
+echo "Please refer to the documentation for you board to properly update it."
